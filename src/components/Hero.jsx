4ec78fcb2dd70ee5d3726b6d6d254d7e30f72992
Om-Govindani <omgovindani@gmail.com>
2024-06-18 16:27:34 +0530

    Final Steps

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ export default function Hero(){
         <div className="min-h-screen flex flex-col gap-10 justify-center items-center max-w-[700px] w-full mx-auto p-4">
             <p>SHAPE YOUR LIFE & BODY</p>
             <h1 className="uppercase font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl ">GYM<span className="bg-gradient-to-r from-sky-500 to-zinc-200 bg-clip-text text-transparent">GUY</span></h1>
-            <p className="text-sm md:text-base font-light text-center">I here by acknowledge that I may become a <span className="text-sky-400 font-medium">GymGuy</span> and Accept the challege of improving my <span className="text-sky-400 font-medium">Lifestyle</span> and <span className="text-sky-400 font-medium">Body</span></p>
+            <p className="text-sm md:text-base font-light text-center">I here by acknowledge that I may become a <span className="text-sky-400 font-medium">GymGuy</span> and Accept the challenge of improving my <span className="text-sky-400 font-medium">Lifestyle</span> and <span className="text-sky-400 font-medium">Body</span></p>
             <Button func={()=>{window.location.href='#generate'}} text={"Accept & Begin"}></Button>
         </div>
     );
